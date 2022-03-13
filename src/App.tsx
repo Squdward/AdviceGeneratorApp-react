82f5dab0194b7c5ab592f919335969f7692be10d
vv_semenov <vv_semenov@betboom.org>
2022-03-13 23:54:32 +0300

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+interface IState {
+  slip: {
+    id: number;
+    advice: string;
+  };
+}
 
 function App() {
+  const [state, setState] = useState<IState>();
+
+  function randomNumber() {
+    return Math.trunc(1 + Math.random() * (200 - 1));
+  }
+
+  async function getData(): Promise<void> {
+    try {
+      const id = randomNumber();
+      const response = await fetch(`https://api.adviceslip.com/advice/${id}`);
+
+      if (response.ok) {
+        const data: IState = await response.json();
+
+        setState(data);
+      } else {
+        throw new Error("Что-то пошло не так");
+      }
+    } catch (err) {
+      console.error(err);
+      getData()
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Wrapper">
+      <div className="Block">
+        <span className="Number">Advice #{state?.slip?.id}</span>
+        <p className="Advice">{state?.slip?.advice}</p>
+        <div className="Decoration"></div>
+        <button className="Generate" onClick={getData}>
+          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
+              fill="#202733"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 }
